0b285e3cd8b9e7b6f1281d307a7cd52962ad514a
davidklofanda-star <davidklofanda@gmail.com>
2026-01-11 21:47:56 +0100

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next';
+import siteMetadata from '@/app/metadata.json';
+import HomePageContent from '@/components/home-page-content';
+
+export const metadata: Metadata = siteMetadata['/'];
+
+export default function Home() {
+  return <HomePageContent />;
+}
